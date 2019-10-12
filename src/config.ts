4a13f5970294d16f1e04534cb962f63263488f18
Jinnan Zhang <vannizhang@gmail.com>
2019-10-12 13:57:29 -0700

    use the real change detection service

--- src/config.ts
+++ src/config.ts
@@ -5,12 +5,12 @@ export default {
         'portal-url': 'https://devext.arcgis.com',
         'wayback-imagery-base': 'https://waybackdev.maptiles.arcgis.com/arcgis/rest/services/World_Imagery/MapServer',
         'wayback-config': 'https://s3-us-west-2.amazonaws.com/config.maptiles.arcgis.com/dev/waybackconfig.json',
-        'wayback-change-detector-layer': 'https://services.arcgis.com/jIL9msH9OI208GCb/arcgis/rest/services/clumps_gdb/FeatureServer/0'
+        'wayback-change-detector-layer': 'https://metadatadev.maptiles.arcgis.com/arcgis/rest/services/Wayback_Footprints/MapServer/0'
     },
     'prod': {
         'portal-url': 'https://www.arcgis.com',
         'wayback-imagery-base': 'https://wayback.maptiles.arcgis.com/arcgis/rest/services/World_Imagery/MapServer',
         'wayback-config': 'https://s3-us-west-2.amazonaws.com/config.maptiles.arcgis.com/waybackconfig.json',
-        'wayback-change-detector-layer': 'https://services.arcgis.com/jIL9msH9OI208GCb/arcgis/rest/services/clumps_gdb/FeatureServer/0'
+        'wayback-change-detector-layer': 'https://metadatadev.maptiles.arcgis.com/arcgis/rest/services/Wayback_Footprints/MapServer/0'
     }
 }
