abc6b47ba393cea2751fc0b9a10a5fd92e10fb2e
Jinnan Zhang <vannizhang@gmail.com>
2019-11-14 14:59:16 -0800

    fix the missing icon bug in IE

--- src/components/CheckboxToggle/index.tsx
+++ src/components/CheckboxToggle/index.tsx
@@ -18,14 +18,14 @@ class CheckboxToggle extends React.PureComponent<IProps> {
         const { isActive, onChange } = this.props;
 
         const iconSvg = isActive ? (
-            <span className="icon-ui-checkbox-checked"></span>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" height="24" width="24"><path d="M19.719 2H3.28A1.281 1.281 0 0 0 2 3.281v16.437A1.282 1.282 0 0 0 3.281 21h16.437A1.282 1.282 0 0 0 21 19.718V3.281A1.281 1.281 0 0 0 19.719 2zM20 19.719a.281.281 0 0 1-.281.281H3.28a.281.281 0 0 1-.28-.281V3.28A.281.281 0 0 1 3.281 3H19.72a.281.281 0 0 1 .281.281zm-14-7.03l.637-.636L9.5 14.727l7.022-6.87.637.637L9.5 16z"/></svg>
         ) : (
-            <span className="icon-ui-checkbox-unchecked text-dark-gray"></span>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" height="24" width="24"><path d="M20.719 3H4.28A1.281 1.281 0 0 0 3 4.281v16.437A1.282 1.282 0 0 0 4.281 22h16.437A1.282 1.282 0 0 0 22 20.718V4.281A1.281 1.281 0 0 0 20.719 3zM21 20.719a.281.281 0 0 1-.281.281H4.28a.281.281 0 0 1-.28-.281V4.28A.281.281 0 0 1 4.281 4H20.72a.281.281 0 0 1 .281.281z"/></svg>
         );
 
         return (
             <div className="checkbox-toggle-control" onClick={onChange}>
-                <span className={`checkbox-icon text-dark-gray`}>
+                <span className={`checkbox-icon margin-right-quarter text-dark-gray`}>
                     {iconSvg}
                 </span>
                 <span className="font-size--2">
--- src/components/Map/ReferenceLayerToggle/index.tsx
+++ src/components/Map/ReferenceLayerToggle/index.tsx
@@ -17,20 +17,20 @@ class ReferenceLayerToggle extends React.PureComponent<IProps> {
         const { isActive, onClick } = this.props;
 
         const icon = isActive ? (
-            <span className="icon-ui-checkbox-checked"></span>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" height="16" width="16"><path d="M13.071 1H1.93a.929.929 0 0 0-.93.929V13.07a.929.929 0 0 0 .929.929H13.07a.929.929 0 0 0 .929-.929V1.93a.929.929 0 0 0-.928-.93zM13 13H2V2h11zM3.735 7.346l.738-.738 2.084 2.088L11.262 4l.738.738-5.443 5.43z"/></svg>
         ) : (
-            <span className="icon-ui-checkbox-unchecked"></span>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" height="16" width="16"><path d="M14.071 2H2.93a.929.929 0 0 0-.93.929V14.07a.929.929 0 0 0 .929.929H14.07a.929.929 0 0 0 .929-.929V2.93a.929.929 0 0 0-.928-.93zM14 14H3V3h11z"/></svg>
         );
 
         return (
             <div className="reference-layer-toggle text-white">
                 <div
-                    className="inline-block margin-left-half cursor-pointer"
+                    className="margin-left-half margin-right-quarter cursor-pointer"
                     onClick={onClick}
                 >
                     {icon}
                 </div>
-                <div className="inline-block">reference label overlay</div>
+                <div className="">reference label overlay</div>
             </div>
         );
     }
