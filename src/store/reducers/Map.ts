32552350a1f4f3ff3acce35d06279ca1d961cf06
Jinnan Zhang <vannizhang@gmail.com>
2020-12-03 11:21:52 -0800

    add Map reducer

---
+++ src/store/reducers/Map.ts
@@ -0,0 +1,47 @@
+import {
+    createSlice,
+    createSelector,
+    PayloadAction,
+    // createAsyncThunk
+} from '@reduxjs/toolkit';
+import { IExtentGeomety, IScreenPoint, IWaybackMetadataQueryResult } from '../../types';
+
+import { RootState, StoreDispatch, StoreGetState } from '../configureStore';
+
+export type MapState = {
+    mapExtent: IExtentGeomety;
+    metadataQueryResult: IWaybackMetadataQueryResult;
+    metadataPopupAnchor: IScreenPoint;
+};
+
+export const initialMapState:MapState = {
+    mapExtent: null,
+    metadataQueryResult: null,
+    metadataPopupAnchor: null
+};
+
+const slice = createSlice({
+    name: 'Map',
+    initialState: initialMapState,
+    reducers: {
+        mapExtentUpdated: (state:MapState, action:PayloadAction<IExtentGeomety>)=>{
+            state.mapExtent = action.payload;
+        },
+        metadataQueryResultUpdated: (state:MapState, action:PayloadAction<IWaybackMetadataQueryResult>)=>{
+            state.metadataQueryResult = action.payload
+        },
+        metadataPopupAnchorUpdated: (state:MapState, action:PayloadAction<IScreenPoint>)=>{
+            state.metadataPopupAnchor = action.payload
+        },
+    }
+});
+
+const { reducer } = slice;
+
+export const {
+    mapExtentUpdated,
+    metadataQueryResultUpdated,
+    metadataPopupAnchorUpdated
+} = slice.actions;
+
+export default reducer;
--- src/store/reducers/rootReducer.ts
+++ src/store/reducers/rootReducer.ts
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import UI from './UI';
+import Map from './Map';
 import SwipeView from './SwipeView';
-import WaybackItems from './WaybackItems'
+import WaybackItems from './WaybackItems';
 
 export default combineReducers({
     UI,
+    Map,
     SwipeView,
     WaybackItems
 });
