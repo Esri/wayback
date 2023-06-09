a4acaa17b0eb9e384ecafd1769930a868bb3cb2a
Jinnan Zhang <vannizhang@gmail.com>
2023-06-08 17:23:36 -0700

    fix: constants alias in webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -53,7 +53,7 @@ module.exports = (env, options)=> {
             extensions: ['.js', '.jsx', '.json', '.ts', '.tsx'],
             alias: {
                 '@components': path.resolve(__dirname, 'src/components/'),
-                '@components': path.resolve(__dirname, 'src/components/'),
+                '@constants': path.resolve(__dirname, 'src/constants/'),
                 '@contexts': path.resolve(__dirname, 'src/contexts/'),
                 '@hooks': path.resolve(__dirname, 'src/hooks/'),
                 '@store': path.resolve(__dirname, 'src/store/'),
