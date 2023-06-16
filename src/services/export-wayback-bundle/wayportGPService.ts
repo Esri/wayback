56deb76dd40f89f634e8feebd40d9354ef1575a0
Jinnan Zhang <vannizhang@gmail.com>
2023-06-16 13:04:40 -0700

    fix: submit wayport job should be use HTTP GET

--- src/services/export-wayback-bundle/wayportGPService.ts
+++ src/services/export-wayback-bundle/wayportGPService.ts
@@ -103,10 +103,9 @@ export const submitJob = async ({
         extent: `${xmin} ${ymin} ${xmax} ${ymax} PROJCS["WGS_1984_Web_Mercator_Auxiliary_Sphere",GEOGCS["GCS_WGS_1984",DATUM["D_WGS_1984",SPHEROID["WGS_1984",6378137.0,298.257223563]],PRIMEM["Greenwich",0.0],UNIT["Degree",0.0174532925199433]],PROJECTION["Mercator_Auxiliary_Sphere"],PARAMETER["False_Easting",0.0],PARAMETER["False_Northing",0.0],PARAMETER["Central_Meridian",0.0],PARAMETER["Standard_Parallel_1",0.0],PARAMETER["Auxiliary_Sphere_Type",0.0],UNIT["Meter",1.0]]`,
     });
 
-    const res = await fetch(`${WAYPORT_GP_SERVICE_ROOT}/submitJob`, {
-        method: 'post',
-        body: params,
-    });
+    const res = await fetch(
+        `${WAYPORT_GP_SERVICE_ROOT}/submitJob?${params.toString()}`
+    );
 
     const data = await res.json();
 
