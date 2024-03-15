96ba6e31608952aaf8cabb44c218cbd505a0d25b
Jinnan Zhang <vannizhang@gmail.com>
2024-03-15 09:23:17 -0700

    fix: add drop-shadow effect to Close Button

--- src/components/CloseButton/CloseButton.tsx
+++ src/components/CloseButton/CloseButton.tsx
@@ -28,7 +28,7 @@ export const CloseButton: FC<Props> = ({ onClick }: Props) => {
                 viewBox="0 0 32 32"
                 height="64"
                 width="64"
-                className="absolute top-1 right-1 cursor-pointer"
+                className="absolute top-1 right-1 cursor-pointer with-drop-shadow"
                 onClick={onClick}
             >
                 <path
