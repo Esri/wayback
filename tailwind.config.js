871747f7d61a010b1fc783a1817c75bfe97ce353
Jinnan Zhang <vannizhang@gmail.com>
2024-06-13 12:56:14 -0700

    chore: revert READE and tailwind.config.js update tailwind.config.js to suppress warning message about deprecated colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,39 +1,46 @@
+const colors = require('tailwindcss/colors')
+
+/**
+ * suppress the warning od deprecated colors.
+ * @see https://github.com/tailwindlabs/tailwindcss/issues/4690#issuecomment-1046087220
+ */
+delete colors['lightBlue'];
+delete colors['warmGray'];
+delete colors['trueGray'];
+delete colors['coolGray'];
+delete colors['blueGray'];
+
 module.exports = {
-  content: [
+  content: [ 
     './src/**/*.html',
-    './src/**/*.{js,jsx,ts,tsx}', 
+    './src/**/*.{js,jsx,ts,tsx}' 
   ],
-  darkMode: 'class', // or 'media' or false
+  darkMode: 'class',
   theme: {
-    extend: {
-      colors: {
-        sky: require('tailwindcss/colors').sky,
-        stone: require('tailwindcss/colors').stone,
-        neutral: require('tailwindcss/colors').neutral,
-        gray: require('tailwindcss/colors').gray,
-        slate: require('tailwindcss/colors').slate,
-        custom: {
-          'modal': {
-            'background': 'rgba(26,61,96, 0.9)',
-            'content-background': '#000'
-          },
-          'theme': {
-            'blue': {
-              DEFAULT: '#2267AE',
-              'brand': '#0079c1',
-              'light': '#56a5d8',
-              'dark': '#1A3D60'
-            }
-          },
-          'background': {
-            DEFAULT: '#121212'
-          },
-          'foreground': {
-            DEFAULT: '#ccc'
+    colors: {
+      ...colors,
+      custom: {
+        'modal': {
+          'background': 'rgba(26,61,96, 0.9)',
+          'content-background': '#000'
+        },
+        'theme': {
+          'blue': {
+            DEFAULT: '#2267AE',
+            'brand': '#0079c1',
+            'light': '#56a5d8',
+            'dark': '#1A3D60'
           }
+        },
+        'background': {
+          DEFAULT: '#121212'
+        },
+        'foreground': {
+          DEFAULT: '#ccc'
         }
-      },
+      }
     },
+    extend: {},
   },
   variants: {
     extend: {},
