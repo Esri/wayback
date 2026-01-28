ee6d3d32b7405f8f6c4f726ebe16dc7f76c42586
Jinnan Zhang <vannizhang@gmail.com>
2026-01-28 11:56:17 -0800

    doc: update documentation comments for PerformanceAnalyzeToolContainer

--- src/components/PerformanceAnalyzeTool/PerformanceAnalyzeToolContainer.tsx
+++ src/components/PerformanceAnalyzeTool/PerformanceAnalyzeToolContainer.tsx
@@ -3,6 +3,15 @@ import { selectIsPerformanceAnalyzeToolEnabled } from '@store/UI/reducer';
 import React from 'react';
 import { LocalChangeQueryDuration } from './LocalChangeQueryDuration';
 
+/**
+ * This container component conditionally renders the Performance Analyze Tool components
+ * based on whether the tool is enabled in the application state.
+ *
+ * To enable the Performance Analyze Tool, set the URL query parameter
+ * `?enablePerformanceAnalyzeTool=true`.
+ *
+ * @returns
+ */
 export const PerformanceAnalyzeToolContainer = () => {
     const isEnabled = useAppSelector(selectIsPerformanceAnalyzeToolEnabled);
 
